0aa013c92b605bc46740d9cc87eb9fde42f34eb7
Dave <david.la.gordt.dillie@gmail.com>
2021-05-13 17:12:07 +0200

    test re-run

--- songify-backend/src/test/java/com/songify/api/servicetests/RoleServiceTests.java
+++ songify-backend/src/test/java/com/songify/api/servicetests/RoleServiceTests.java
@@ -4,6 +4,7 @@ import com.songify.api.model.Role;
 import com.songify.api.model.dto.RoleDto;
 import com.songify.api.service.RoleService;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,15 +19,20 @@ class RoleServiceTests {
     @Autowired
     RoleService roleService;
 
+    void createTestData(){
+        roleService.addRole(new RoleDto(1L, "test"));
+        roleService.addRole(new RoleDto(2L, "test2"));
+        roleService.addRole(new RoleDto(3L, "name"));
+        roleService.addRole(new RoleDto(4L, "no"));
+    }
+
     @Test
     void getAllRolesTest(){
-        roleService.addRole(new RoleDto(1L, "test"));
-        roleService.addRole(new RoleDto(2L, "testRole"));
-
+        createTestData();
         List<Role> roles = roleService.getRoles().getBody();
 
         Assertions.assertEquals(roles.get(0).getName(), "test");
-        Assertions.assertEquals(roles.get(1).getName(), "testRole");
+        Assertions.assertEquals(roles.get(1).getName(), "test2");
     }
 
     @Test
@@ -34,9 +40,8 @@ class RoleServiceTests {
         Role role = new Role();
         role.setId(4L);
         role.setName("no");
-        roleService.addRole(new RoleDto(4L, "no"));
 
-        String roleName = roleService.getRoleById(1L).getBody().getName();
+        String roleName = roleService.getRoleById(4L).getBody().getName();
 
         Assertions.assertEquals(roleName, role.getName());
     }
@@ -44,8 +49,6 @@ class RoleServiceTests {
     @Test
     void updateRoleTest(){
         RoleDto updatedRole = new RoleDto(1L, "Name");
-
-        roleService.addRole(new RoleDto(1L, "test"));
 
         roleService.updateRole(1L, updatedRole);
 
