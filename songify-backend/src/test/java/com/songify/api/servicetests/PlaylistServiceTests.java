d05a878aaa5f718f5c24a492a228bf6efb0cc419
Dave <david.la.gordt.dillie@gmail.com>
2021-05-14 13:08:41 +0200

    t

--- songify-backend/src/test/java/com/songify/api/servicetests/PlaylistServiceTests.java
+++ songify-backend/src/test/java/com/songify/api/servicetests/PlaylistServiceTests.java
@@ -28,10 +28,10 @@ class PlaylistServiceTests {
 
     @Test
     void getPlaylistsTest(){
-        playlistService.addPlaylist(new NewPlaylistRequest("playlist name", "description!"));
+        playlistService.addPlaylist(new NewPlaylistRequest("yep", "description!"));
         List<Playlist> playlists = playlistService.getPlaylists();
 
-        Assertions.assertEquals("playlist name", playlists.get(0).getTitle());
+        Assertions.assertEquals("yep", playlists.get(0).getTitle());
     }
 
     @Test
