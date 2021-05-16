b2450d7476f9e73704ec415b2fa3a6fdc913fc7e
Dave <david.la.gordt.dillie@gmail.com>
2021-05-16 13:55:00 +0200

    more tests

--- songify-backend/src/test/java/com/songify/api/model/AuditableTests.java
+++ songify-backend/src/test/java/com/songify/api/model/AuditableTests.java
@@ -2,19 +2,56 @@ package com.songify.api.model;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.Date;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@SpringBootTest
 class AuditableTests {
 
     @Test
     void auditableGetAndSetCreatedByTest(){
         Auditable<String> auditableClass = Mockito.mock(
                 Auditable.class,
-            Mockito.CALLS_REAL_METHODS);
+                Mockito.CALLS_REAL_METHODS);
 
         auditableClass.setCreatedBy("me");
         assertEquals("me", auditableClass.getCreatedBy());
+    }
 
+    @Test
+    void auditableGetAndSetCreatedDateTest(){
+        Auditable<String> auditableClass = Mockito.mock(
+                Auditable.class,
+                Mockito.CALLS_REAL_METHODS);
+
+        Date date = new Date();
+        date.setTime(2L);
+        auditableClass.setCreatedDate(date);
+        assertEquals(2L, auditableClass.getCreatedDate().getTime());
+    }
+
+    @Test
+    void auditableGetAndSetLastModifiedByTest(){
+        Auditable<String> auditableClass = Mockito.mock(
+                Auditable.class,
+                Mockito.CALLS_REAL_METHODS);
+
+        auditableClass.setLastModifiedBy("me");
+        assertEquals("me", auditableClass.getLastModifiedBy());
+    }
+
+    @Test
+    void auditableGetAndSetLastModifiedDateTest(){
+        Auditable<String> auditableClass = Mockito.mock(
+                Auditable.class,
+                Mockito.CALLS_REAL_METHODS);
+
+        Date date = new Date();
+        date.setTime(2L);
+        auditableClass.setLastModifiedDate(date);
+        assertEquals(2L, auditableClass.getLastModifiedDate().getTime());
     }
 }
---
+++ songify-backend/src/test/java/com/songify/api/model/SongTests.java
@@ -0,0 +1,29 @@
+package com.songify.api.model;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SongTests {
+
+    @Test
+    void getAndSetIdTest(){
+        Song song = new Song(2);
+        song.setId(2L);
+        Assertions.assertEquals(2L, song.getId());
+    }
+
+    @Test
+    void getSpotifyIdTest(){
+        Song song = new Song(2);
+        Assertions.assertEquals(2, song.getSpotifyId());
+    }
+
+    @Test
+    void setSpotifyIdTest(){
+        Song song = new Song(2);
+        song.setSpotifyId(3);
+        Assertions.assertEquals(3, song.getSpotifyId());
+    }
+}
