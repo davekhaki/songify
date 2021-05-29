a751342d39888354b16043d610cbebbcc329e917
Dave <david.la.gordt.dillie@gmail.com>
2021-05-29 12:37:09 +0200

    tidy app.js

--- songify-frontend/src/App.js
+++ songify-frontend/src/App.js
@@ -1,37 +1,23 @@
 import './App.css';
-
 import React from 'react';
-
 import Navbar from './components/shared/nav.bar.component';
-
-import Image from './img/logo.png';
-
-import AuthService from './services/auth/auth.service';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentUser: undefined,
     };
 
   }
 
   componentDidMount() {
-    const user = AuthService.getCurrentUser();
 
-    if (user) {
-      this.setState({
-        currentUser: user,
-      });
-    }
   }
 
   render() {
     return (
       <div className="App">
         <Navbar />
-        {/* <PopularPlaylist></PopularPlaylist> */}
       </div>
     );
   }
