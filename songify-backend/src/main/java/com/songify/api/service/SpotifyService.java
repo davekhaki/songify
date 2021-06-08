57bff105274fc626d9c2de5696a6c90e0c993301
Dave <david.la.gordt.dillie@gmail.com>
2021-06-08 14:12:01 +0200

    remove public modifier in interface method

--- songify-backend/src/main/java/com/songify/api/service/SpotifyService.java
+++ songify-backend/src/main/java/com/songify/api/service/SpotifyService.java
@@ -3,5 +3,5 @@ package com.songify.api.service;
 import javax.servlet.http.HttpServletResponse;
 
 public interface SpotifyService {
-    public void authenticate(String code, HttpServletResponse httpServletResponse);
+    void authenticate(String code, HttpServletResponse httpServletResponse);
 }
